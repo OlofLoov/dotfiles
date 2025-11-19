88d059672830c153d886fc0ada83586f4e2d2685
Olof Lööv <elskurkos@protonmail.com>
2025-11-19 11:40:35 +0000

    clean after helm

--- zsh/tools/04_helm.zsh
+++ zsh/tools/04_helm.zsh
@@ -2,3 +2,5 @@ curl -LO https://get.helm.sh/helm-v3.15.0-linux-amd64.tar.gz
 tar -zxvf helm-v3.15.0-linux-amd64.tar.gz
 chmod +x linux-amd64/helm
 mv linux-amd64/helm "$HOME/.dotfiles/bin/helm"
+rm helm-v3.15.0-linux-amd64.tar.gz
+rm -rf linux-amd64/
